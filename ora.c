fc0903bc6b8f70295b0586a58855dfbfdbd78e54
azyrus66 <azyrus66@gmail.com>
2018-12-02 01:36:29 +0100

    Set time with keyboard TODO: edge controlled keyboard

--- ora.c
+++ ora.c
@@ -203,7 +203,6 @@ void keyboard_in(void) {
 		}
 		
 	}
-<<<<<<< HEAD
 }
 
 void input_num(uint16_t max) {
@@ -280,24 +279,7 @@ void set_time(void) {
 			}
 			break;
 		}
-=======
-	if (c && sor!=4) {
-		return (oszlop+3*(sor-1));
-	}
-	else if (c && sor==4) {
-		if (oszlop==1) {
-			return '*';
-		}
-		else if (oszlop==2) {
-			return 0;
-		}
-		else if (oszlop==3) {
-			return '#';
-		}
-	}
-	else {
-		return 0;
->>>>>>> 18de3b95006ee6858430a0a534a7b6e1f3bee6de
+
 	}
 }
 
